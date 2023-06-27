e02fb11fbb53bada4d80bfec7c3ab91a27c63abb
Marcos <marcoswinmendes@gmail.com>
2023-06-27 20:17:12 -0300

    Moving pieces

--- src/application/UI.java
+++ src/application/UI.java
@@ -1,7 +1,11 @@
 package application;
 
 import chess.ChessPiece;
+import chess.ChessPosition;
 import chess.Color;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class UI {
     public static final String ANSI_RESET = "\u001B[0m";
@@ -23,6 +27,19 @@ public class UI {
     public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
     public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
+
+    public static ChessPosition readChessPosition(Scanner sc){
+        try{
+            String s = sc.nextLine();
+            char column = s.charAt(0);
+            int row = Integer.parseInt(s.substring(1));
+            return new ChessPosition(column, row);
+        }
+        catch(RuntimeException e){
+                throw new InputMismatchException("Error! possible position at a1 to h8");
+        }
+    }
+
     public static void printBoard(ChessPiece[][] pieces) {
         for (int i = 0; i < pieces.length; i++) {
             System.out.print((8 - i) + " ");
@@ -35,13 +52,6 @@ public class UI {
     }
 
     private static void printPiece(ChessPiece piece) {
-        if (piece == null) {
-            System.out.print("-");
-        }
-        else {
-            System.out.print(piece);
-        }
-        System.out.print(" ");
         if (piece == null) {
             System.out.print("-");
         }
